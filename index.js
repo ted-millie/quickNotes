545c6d9f5e1d16d1b8f42f135e2a68d2c9f896a4
ted-millie <ted@millie.town>
2022-01-11 23:52:40 +0900

    모멘텀 애니메이션 추가

--- index.js
+++ index.js
@@ -16,8 +16,10 @@
   let translateX = 0
   let translateY = 0
 
-  let startTime = 0
-  let endTime = 0
+  let velX = 0
+  let velY = 0
+
+  let isDown = false
 
   function init () {
     initEvents()
@@ -25,47 +27,69 @@
 
   function initEvents () {
     nodes.experimentHeader.addEventListener('pointerdown', onPointerDown)    
-    nodes.experimentHeader.addEventListener('pointerup', onPointerUp)
 
     document.addEventListener('dragover', event => event.preventDefault())
   }
 
   function onPointerDown (event) {
+    isDown = true
     mouseX = event.clientX
     mouseY = event.clientY
-    startTime = Date.now()
 
     nodes.experimentHeader.setPointerCapture(event.pointerId)
     nodes.experimentHeader.addEventListener('pointermove', onPointerMove)
+    nodes.experimentHeader.addEventListener('pointerup', onPointerUp)
+    nodes.experimentHeader.addEventListener('pointerout', onPointerUp)
+    // pointerup pointercancel pointerout pointerleave
   }
 
   function onPointerMove (event) {
     const deltaX = event.clientX - mouseX
     const deltaY = event.clientY - mouseY
+    const prevTranslateX = translateX
+    const prevTranslateY = translateY
     translateX = elementX + deltaX
     translateY = elementY + deltaY
+    velX = translateX - prevTranslateX
+    velY = translateY - prevTranslateY
 
-    requestAnimationFrame(move)
+    move(translateX, translateY)
   }
 
   function onPointerUp (event) {
+    if (!isDown) {
+      return
+    }
+
+    isDown = false
     elementX = translateX
     elementY = translateY
-    endTime = Date.now()
 
-    requestAnimationFrame(easing)
+    momentum()
 
     nodes.experimentHeader.releasePointerCapture(event.pointerId)
     nodes.experimentHeader.removeEventListener('pointermove', onPointerMove)
+    nodes.experimentHeader.removeEventListener('pointerup', onPointerUp)
+    nodes.experimentHeader.removeEventListener('pointerout', onPointerUp)
   }
 
-  function easing () {
-    const timeElapsed = Date.now() - endTime
-    console.log(timeElapsed)
+  function momentum () {
+    translateX += velX
+    translateY += velY
+    elementX = translateX
+    elementY = translateY
+    velX *= 0.95
+    velY *= 0.95
+
+    move(translateX, translateY)
+
+    if (Math.abs(velX) > 0.5 || Math.abs(velY) > 0.5) {
+      requestAnimationFrame(momentum)
+    }
   }
 
-  function move () {
-    nodes.experimentRoot.style.transform = `translate(${translateX}px, ${translateY}px)`
+  function move (x, y) {
+    nodes.experimentRoot.style.transform = `translate(${x}px, ${y}px)`
   }
 
   init()
